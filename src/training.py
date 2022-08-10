18bcb962f9b631a36d761f00a2f0b8d86bb3c17d
SahilRaheja <sahil.raheja5010@gmail.com>
2022-08-10 11:42:18 +0530

    Saved model using function and time

--- src/training.py
+++ src/training.py
@@ -1,36 +1,48 @@
 from utils.common import read_config
 from utils.data_mgmt import get_data
 from utils.model import create_model
+from utils.model import save_model
+import os
 
 import argparse
+
 
 def training(config_path):
     config = read_config(config_path)
 
     validation_data_size = config["params"]["validation_data_size"]
 
-    (X_train, y_train), (X_valid, y_valid ), (X_test, y_test) = get_data(validation_data_size)
+    (X_train, y_train), (X_valid, y_valid), (X_test, y_test) = get_data(validation_data_size)
 
-    LOSS_FUNCTION=config["params"]["loss_function"]
-    OPTIMIZER=config["params"]["optimizer"]
-    METRICS=config["params"]["metrics"]
-    EPOCHS=config["params"]["epochs"]
-    no_classes=config["params"]["no_classes"]
-    input_shape=config["params"]["input_shape"]
-  
+    LOSS_FUNCTION = config["params"]["loss_function"]
+    OPTIMIZER = config["params"]["optimizer"]
+    METRICS = config["params"]["metrics"]
+    EPOCHS = config["params"]["epochs"]
+    no_classes = config["params"]["no_classes"]
+    input_shape = config["params"]["input_shape"]
+
     model = create_model(LOSS_FUNCTION, OPTIMIZER, METRICS, EPOCHS, no_classes, input_shape)
 
     VALIDATION_SET = (X_valid, y_valid)
 
-    history = model.fit(X_train, y_train, epochs = EPOCHS, validation_data=VALIDATION_SET)
+    history = model.fit(X_train, y_train, epochs=EPOCHS, validation_data=VALIDATION_SET)
+
+    artifacts_dir = config["artifacts"]["artifacts_dir"]
+    model_dir = config["artifacts"]["model_dir"]
+
+    model_dir_path = os.path.join(artifacts_dir, model_dir)
+    os.makedirs(model_dir_path, exist_ok=True)
+
+    model_name = config["artifacts"]["model_name"]
+
+    save_model(model, model_name, model_dir_path)
+
 
 if __name__ == '__main__':
-
     args = argparse.ArgumentParser()
 
-    args.add_argument("--config","-c", default="config.yaml")
+    args.add_argument("--config", "-c", default="config.yaml")
 
     parsed_args = args.parse_args()
 
-    training(config_path = parsed_args.config)
-
+    training(config_path=parsed_args.config)
